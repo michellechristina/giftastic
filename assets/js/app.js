24e3c2ca70270f402664d91ceacf85b35db2cb3c
michellechristina <michelle@thisisbam.com>
2017-10-27 22:57:12 -0400

    css added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
-var gifs = ["Apple", "Pepper", "Pizza", "Milk"];
+var gifs = ["Werewolf", "Ghost", "Witch", "Chupacabra", "Vampire"];
+
+$("#gifContent").hide();
 
 // displayImageContent function re-renders the HTML to display the appropriate content
 function displayImageContent() {
@@ -6,6 +8,8 @@ function displayImageContent() {
     var gif = $(this).attr("data-name");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         gif + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+       
 
     // Creating an AJAX call for the specific button being clicked
     $.ajax({
@@ -16,12 +20,16 @@ function displayImageContent() {
         var results = response.data;
         console.log(results);
 
+        // this clears out the gifs when you click on a new button, instead of just appending them to the top of the stack
+        $("#gifContent").html("");
+        // document.getElementById("gifContent").innerHTML="";
+
         // Looping through each result item
         for (var i = 0; i < results.length; i++) {
 
-            var animated = response.data[i].images.fixed_height_still.url;
+            var animated = response.data[i].images.fixed_height.url;
             var still = response.data[i].images.fixed_height_still.url;
-        
+
 
             // Creating and storing a div tag
             var gifDiv = $("<div>");
@@ -35,7 +43,7 @@ function displayImageContent() {
             gifImage.attr('src', still);
             gifImage.attr('data-still', still);
             gifImage.attr('data-animated', animated);
-            gifImage.attr("data-state", 'still');
+            gifImage.attr("data-state", 'animated');
             gifImage.addClass('theImage');
             // Setting the src attribute of the image to a property pulled off the result item
             gifImage.attr("src", results[i].images.fixed_height.url);
@@ -45,29 +53,34 @@ function displayImageContent() {
             gifDiv.append(p);
             
 
-            // Prependng the gifDiv to the HTML page in the "#gifs-appear-here" div
+            // Prependng the gifDiv to the HTML page in the "#gifsContent" div
+
             $("#gifContent").prepend(gifDiv);
+            $("#gifContent").show();
         }
+
+
     });
 
 
-        $(document).on('click', '.theImage', function(){
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    $(document).on('click', '.theImage', function () {
+        console.log($(this));
+        //  get or set the value of any attribute on HTML element
         var state = $(this).attr("data-state");
         // If the clicked image's state is still, update its src attribute to what its data-animate value is.
         // Then, set the image's data-state to animate
         // Else set src to the data-still value
         if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animated"));
-          $(this).attr("data-state", "animated");
+            $(this).attr("src", $(this).attr("data-animated"));
+            $(this).attr("data-state", "animated");
 
-          console.log($(this).attr("animated " +"data-animated"));
+            console.log("animated " + $(this).attr("data-animated"));
         } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-          console.log($(this).attr("stil image " +"data-still"));
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+            console.log('still ' + $(this).attr("data-still"));
         }
-      });
+    });
 
 }
 
@@ -98,6 +111,7 @@ function renderButtons() {
 // This function handles events where a gif button is clicked
 $("#add-gif").on("click", function (event) {
     event.preventDefault();
+   
     // This line grabs the input from the textbox
     var gif = $("#search-input").val();
 
@@ -106,6 +120,9 @@ $("#add-gif").on("click", function (event) {
 
     // Calling renderButtons which handles the processing of our gif array
     renderButtons();
+
+     //this clears out the input text box after you click submit
+     $("#search-input").val("");
 });
 
 // Adding a click event listener to all elements with a class of "gif"
